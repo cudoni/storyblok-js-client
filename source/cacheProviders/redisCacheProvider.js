82b68ccf4601df2ca1a3a63e1722d000dd40a70b
Daniel <noeldavies@cybershade.org>
2021-12-17 11:40:34 +0000

    Add Redis Cache Provider

---
+++ source/cacheProviders/redisCacheProvider.js
@@ -0,0 +1,48 @@
+import { createClient } from 'redis';
+
+const RedisCacheProvider = {
+  client: null,
+  prefix: 'blok',
+  
+  async connect (config) {
+    let clientOptions = {
+      uri: null
+    }
+
+    if (!config.uri) throw new Error(`Redis connection URI is required ${JSON.stringify(config)} specified`);
+
+    this.client = createClient(clientOptions);
+    this.client.on('error', (err) => console.log('Redis Client Error', err));
+    return this.client.connect();
+  },
+
+  async get(key) {
+    let value = await this.client.hGet(this.prefix, key);
+    return this.deserialize(value);
+  },
+
+  async getAll() {
+    let result = await this.client.hGetAll(this.prefix);
+    return result
+  },
+
+  async set (key, content) {
+    // memory[key] = content;
+    await this.client.hSet(this.prefix, key, this.serialize(content));
+  },
+
+  serialize (value) {
+    if (typeof value === 'string') return value;
+    return JSON.stringify(value);
+  },
+
+  deserialize (value) {
+    return JSON.parse(value);
+  },
+  
+  flush() {
+    this.client.hFlush();
+  }
+}
+
+export default RedisCacheProvider;
